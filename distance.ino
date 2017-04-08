d2d88ee80b48cd4ae390b3add758ceeeee987eb5
gamebird96 <raj.dutta0810@gmail.com>
2017-04-09 00:11:42 +0530

    Add files via upload

---
+++ distance.ino
@@ -0,0 +1,29 @@
+int trig=9;
+int echo=10;
+
+int data=0;
+int distance=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  digitalWrite(trig,LOW);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  
+  data=pulseIn(echo,HIGH);
+  distance=data*0.034/2;
+  Serial.print("Distance=");
+  Serial.println(distance);
+  delay(250);
+}
