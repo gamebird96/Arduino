e7d41c8da19c0fd60dbc01c01ab25dd7e71144b9
gamebird96 <raj.dutta0810@gmail.com>
2017-04-09 00:05:00 +0530

    Add files via upload
    Reads data from GY61 sensor and converts it into equivalent cartesian angle...
    And Simply Displays the data on the serial monitor

---
+++ gy61.ino
@@ -0,0 +1,42 @@
+int xAxis=A0;
+int yAxis=A1;
+int zAxis=A2;
+int xVal,yVal,zVal;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  gyroInit();
+}
+
+void loop() {
+ gyroRead();
+
+}
+void gyroInit()
+{
+    pinMode(xAxis,INPUT);
+  pinMode(yAxis,INPUT);
+  pinMode(zAxis,INPUT);
+  
+  }
+
+void gyroRead()
+{
+   xVal=analogRead(xAxis);
+ yVal=analogRead(yAxis);
+ zVal=analogRead(zAxis);
+ xVal= map(xVal,260,392,0,180);
+ yVal= map(yVal,260,392,0,180);
+ zVal=map(zVal,260,392,0,180);
+  // put your main code here, to run repeatedly:
+  Serial.print(xVal);
+  Serial.print("     ");
+  Serial.print(yVal);
+  Serial.print("     ");
+  Serial.println(zVal);
+  delay(1);
+ }
